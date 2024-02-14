a8e784d99846cd750a789ea6d9d3a1b2fbdcb405
Student <Student@DESKTOP-O0DAQBK>
2024-02-14 14:18:46 +0800

    Activity 3

---
+++ src/activity/pkg3/Activity3.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package activity.pkg3;
+
+/**
+ *
+ * @author Student
+ */
+public class Activity3 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+         double c = 31f;
+       double a = 1.8f;
+       double b = 32f;
+       
+       double result = c * a + b;
+        System.out.println(c*a+b);
+    }
+    
+}
